712a24f3d372b56e0045e1b28f3fa6a38128ea9a
Vasish Baungally <vbaungally@mozilla.com>
2023-06-27 19:11:32 -0500

    added search and refresh

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from mastodon import Mastodon
 
-from helpers import write_html, grab_links
+from helpers import write_html, grab_links, search
 from classifiers import classify_image, classify_video
 
 all_classes = {"adult content", "spoof", "medical", "violence", "racy", "war", 'politics'}
@@ -19,18 +19,40 @@ image_threshold = st.number_input('Image threshold', value=18)
 video_threshold = st.number_input('Video threshold', value=18)
 
 mastodon_social_feed = []
+temp_mastodon_social_feed = []
+MASTODON_FEED = 'MASTODON_FEED'
+MASTODON_FEED_TEMP = 'MASTODON_FEED_TEMP'
+SEARCH = 'search'
+
+st.set_page_config(layout="wide")
+
+
 with st.spinner('Loading Mastodon.social federated feed'):
-    Mastodon.create_app(
-        'hack-posttoots',
-        api_base_url='https://mastodon.social',
-        scopes=['read', 'write'],
-        to_file='.secrets'
-    )
-    mastodon = Mastodon(
-        client_id='.secrets',
-    )
-    mastodon_social_feed = mastodon.timeline_public(only_media=True)
-    st.success(f'Found {len(mastodon_social_feed)} posts!')
+    # run this only once to create the secrets file
+    if MASTODON_FEED not in st.session_state:
+        Mastodon.create_app(
+            'hack-posttoots',
+            api_base_url='https://mastodon.social',
+            scopes=['read', 'write'],
+            to_file='.secrets'
+        )
+        mastodon = Mastodon(
+            client_id='.secrets',
+        )
+        mastodon_social_feed = mastodon.timeline_public(only_media=True, limit=10)
+        st.session_state[MASTODON_FEED] = mastodon_social_feed
+        st.session_state[MASTODON_FEED_TEMP] = mastodon_social_feed.copy()
+        st.session_state[SEARCH] = ''
+
+mastodon = Mastodon(
+    client_id='.secrets',
+)
+
+if st.button(label='Refresh data', key='refresh'):
+    mastodon_social_feed = mastodon.timeline_public(only_media=True, limit=10)
+    st.session_state[MASTODON_FEED] = mastodon_social_feed
+    st.session_state[MASTODON_FEED_TEMP] = mastodon_social_feed.copy()
+    st.experimental_rerun()
 
 
 def render_feed_with_predictions(all_data, limit):
@@ -49,10 +71,10 @@ def render_feed_with_predictions(all_data, limit):
             urls = grab_links(data)
             image_urls = urls['image_urls']
             video_urls = urls['video_urls']
-            if not image_urls and not video_urls:
+            if (not image_urls and not video_urls) or len(image_urls) == 0:
                 continue
             if j == 0:
-                col.markdown(content_html, unsafe_allow_html=True)
+                col.markdown(content_html if len(content_html) > 0 else 'No content', unsafe_allow_html=True)
             elif j == 1:
                 col.markdown(f'<img src={image_urls[0]} alt="" width="250" height="250"> ', unsafe_allow_html=True)
             elif j == 2:
@@ -64,11 +86,18 @@ def render_feed_with_predictions(all_data, limit):
                     if scores:
                         col.write(str(scores))
 
-        write_html('<hr/>')
+        write_html('<br/>')
 
 
-def render(data):
-    render_feed_with_predictions(data, 10)
+def render():
+    with st.form(key='Search'):
+        search_string = ''
+        search_string = st.text_input(f'Enter text to search ({len(st.session_state[MASTODON_FEED])} posts)', search_string)
+        st.session_state[SEARCH] = search_string
+        corpus_submit_button = st.form_submit_button(label='Search')
+        if corpus_submit_button or search_string:
+            st.session_state[MASTODON_FEED_TEMP] = search(st.session_state[MASTODON_FEED], search_string)
+        render_feed_with_predictions(st.session_state[MASTODON_FEED_TEMP], 10)
 
 
-render(mastodon_social_feed)
+render()
